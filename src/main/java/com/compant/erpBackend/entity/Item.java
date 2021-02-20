694d7dcf87c4d587e052650300f5ec476c1009e1
Chaos <chaos87pl@gmail.com>
2021-02-20 08:28:26 +0100

    fix connect item to warehouse one to many

--- src/main/java/com/compant/erpBackend/entity/Item.java
+++ src/main/java/com/compant/erpBackend/entity/Item.java
@@ -23,11 +23,7 @@ public class Item {
     @JoinColumn(name = "idQuantityType")
     private QuantityType quantityType;
 
-    @ManyToMany
-    @JoinTable(
-            name = "item_warehouse",
-            joinColumns = @JoinColumn(name = "idItem"),
-            inverseJoinColumns = @JoinColumn(name = "idWarehouse")
-    )
-    private Set<Warehouse> warehouse;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "idWarehouse")
+    private Warehouse warehouse;
 }
