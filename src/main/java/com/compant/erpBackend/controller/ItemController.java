58d939c96fd3c6444cc55e83af15d2879d3934a0
Chaos <chaos87pl@gmail.com>
2021-02-20 15:32:36 +0100

    edit item

--- src/main/java/com/compant/erpBackend/controller/ItemController.java
+++ src/main/java/com/compant/erpBackend/controller/ItemController.java
@@ -2,11 +2,13 @@ package com.compant.erpBackend.controller;
 
 import com.compant.erpBackend.dto.EmployeeDto;
 import com.compant.erpBackend.dto.ItemDto;
+import com.compant.erpBackend.dto.ItemEditViewDto;
 import com.compant.erpBackend.dto.ItemSaveDto;
 import com.compant.erpBackend.entity.Employee;
 import com.compant.erpBackend.entity.Item;
 import com.compant.erpBackend.repository.EmployeeRepository;
 import com.compant.erpBackend.repository.ItemRepository;
+import com.compant.erpBackend.repository.QuantityTypeRepository;
 import com.compant.erpBackend.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -24,9 +26,21 @@ public class ItemController {
 
     private final ItemService itemService;
 
+    private final QuantityTypeRepository quantityTypeRepository;
+
     @PostMapping("/items")
     public ItemDto newItem(@RequestBody ItemSaveDto dto) {
-        return ItemDto.of(itemService.saveItem(dto));
+        if(dto.getIdItem() == null) {
+            return ItemDto.of(itemService.saveItem(dto));
+        } else {
+            Item item = itemRepository.findById(dto.getIdItem()).get();
+            item.setName(dto.getName());
+            item.setQuantity(dto.getQuantity());
+            item.setQuantityType(quantityTypeRepository.findById(dto.getIdQuantityType()).get());
+            return ItemDto.of(itemRepository.save(item));
+        }
+
+
     }
 
     @GetMapping("/items")
@@ -39,6 +53,13 @@ public class ItemController {
         Optional<Item> optionalItem = itemRepository.findById(idItem);
 
         return ItemDto.of(optionalItem.get());
+    }
+
+    @GetMapping("/item_edit_data/{idItem}")
+    public ItemEditViewDto getItemEditDto(@PathVariable Long idItem) {
+        Item item = itemRepository.findById(idItem).get();
+        ItemEditViewDto dto = ItemEditViewDto.of(item, quantityTypeRepository.findAll());
+        return dto;
     }
 
     @DeleteMapping("/items")
---
+++ src/main/java/com/compant/erpBackend/dto/ItemEditViewDto.java
@@ -0,0 +1,28 @@
+package com.compant.erpBackend.dto;
+
+import com.compant.erpBackend.entity.Item;
+import com.compant.erpBackend.entity.QuantityType;
+import lombok.Data;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+public class ItemEditViewDto {
+
+    private Long idItem;
+    private String name;
+    private Double quantity;
+    private Long idQuantityType;
+    private List<QuantityTypeDto> quantityTypeDtoList;
+
+    public static ItemEditViewDto of(Item item, List<QuantityType> quantityTypeList) {
+        ItemEditViewDto dto = new ItemEditViewDto();
+        dto.setIdItem(item.getIdItem());
+        dto.setName(item.getName());
+        dto.setQuantity(item.getQuantity());
+        dto.setIdQuantityType(item.getQuantityType().getIdQuantityType());
+        dto.setQuantityTypeDtoList(quantityTypeList.stream().map(QuantityTypeDto::of).collect(Collectors.toList()));
+        return dto;
+    }
+}
